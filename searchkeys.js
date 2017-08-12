a0e20169607c82687bf38db01b436b409a2c0f25
Beyang Liu <beyang@sourcegraph.com>
2017-08-12 01:06:56 -0700

    manifest and content script

---
+++ searchkeys.js
@@ -0,0 +1,71 @@
+(function() {
+    /**
+     * State
+     */
+    let selected = -1;
+    let results = [];
+
+    /**
+     * CSS
+     */
+    let css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = '.quick-selected::before { content: "▶"; position: absolute; left: -1em; font-size: 0.75em; top: 0.125em; }';
+    document.body.appendChild(css);
+
+    function isActive() {
+        return document.activeElement.tagName !== "INPUT";
+    }
+
+    /**
+     * Sync view to state
+     */
+    function refresh() {
+        for (let i = 0; i < results.length; i++) {
+            if (i === selected) {
+                results[i].classList.add("quick-selected");
+                if (!isInViewport(results[i])) {
+                    results[i].scrollIntoView(true);
+                }
+            } else {
+                results[i].classList.remove("quick-selected");
+            }
+        }
+    }
+
+    function isInViewport(element) {
+        var rect = element.getBoundingClientRect();
+        var html = document.documentElement;
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || html.clientHeight) &&
+            rect.right <= (window.innerWidth || html.clientWidth)
+        );
+    }
+
+    document.addEventListener('keydown', function (event) {
+        if (!isActive()) {
+            return;
+        }
+
+        if (event.key === "Tab" || event.key === "ArrowUp" || event.key === "ArrowDown") {
+            event.preventDefault();
+            results = document.querySelector("#ires").querySelectorAll(".r a[href]");
+            if (results.length === 0) {
+                return;
+            }
+            if ((event.key === "Tab" && event.shiftKey) || event.key === "ArrowUp") {
+                selected = (selected - 1 + results.length) % results.length;
+            } else if ((event.key === "Tab" && !event.shiftKey) || event.key === "ArrowDown") {
+                selected = (selected + 1 + results.length) % results.length;
+            }
+            refresh();
+        } else if (event.key === "Enter") {
+            if (results.length === 0) {
+                return;
+            }
+            window.location.href = results[selected].href;
+        }
+    }, false);
+})();
