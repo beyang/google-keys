82d027fe78835c07867a2f2b43a95537572332a2
Beyang Liu <beyang@sourcegraph.com>
2018-07-26 10:41:42 -0700

    work for PRs, too

--- gmail.js
+++ gmail.js
@@ -1,7 +1,7 @@
 (function() {
   document.addEventListener('keyup', (event) => {
     if (event.key === "V" && event.ctrlKey && !event.altKey && !event.metaKey && event.shiftKey) {
-      let url = Array.prototype.slice.call(document.querySelectorAll('a[href*="/issues/"]')).pop();
+      let url = Array.prototype.slice.call(document.querySelectorAll('a[href^="https://github.com/"]')).filter(a => a.href.indexOf("/issues/") >= 0 || a.href.indexOf("/pull/") >= 0).pop();
       if (url) {
         window.open(url, "_blank");
       }
