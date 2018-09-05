1da13e0579582f54b3011708efa31f39c4c7dbd8
Beyang Liu <beyang@sourcegraph.com>
2018-09-05 11:44:05 -0700

    support Sourcegraph URLs

--- gmail.js
+++ gmail.js
@@ -1,9 +1,23 @@
+function getGitHubURL() {
+  return Array.prototype.slice.call(document.querySelectorAll('a[href^="https://github.com/"]')).filter(a => a.href.indexOf("/issues/") >= 0 || a.href.indexOf("/pull/") >= 0).pop();
+}
+
+function getSourcegraphURL() {
+  return Array.prototype.slice.call(document.querySelectorAll('a[href^="https://sourcegraph"]')).filter(a => a.innerText.toLowerCase() === "view it on sourcegraph").pop();
+}
+
 (function() {
   document.addEventListener('keyup', (event) => {
     if (event.key === "V" && event.ctrlKey && !event.altKey && !event.metaKey && event.shiftKey) {
-      let url = Array.prototype.slice.call(document.querySelectorAll('a[href^="https://github.com/"]')).filter(a => a.href.indexOf("/issues/") >= 0 || a.href.indexOf("/pull/") >= 0).pop();
-      if (url) {
-        window.open(url, "_blank");
+      const sgURL = getSourcegraphURL();
+      if (sgURL) {
+        window.open(sgURL, "_blank");
+        return;
+      }
+      const ghURL = getGitHubURL();
+      if (ghURL) {
+        window.open(ghURL, "_blank");
+        return;
       }
     }
   });
