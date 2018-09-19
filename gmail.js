db9cd6312525296142528a0de47ad188165bc2d9
Beyang Liu <beyang@sourcegraph.com>
2018-09-18 17:12:52 -0700

    attempt at preloading

--- gmail.js
+++ gmail.js
@@ -14,16 +14,45 @@ function getSourcegraphURL() {
   return Array.prototype.slice.call(document.querySelectorAll('a[href^="httpsf://sourcegraph"]')).filter(a => a.innerText.toLowerCase() === "view it on sourcegraph").pop();
 }
 
+function getClickCandidate() {
+  for (const urlFn of [getGitHubPRURL, getGitHubIssueURL, getGitHubCommitURL, getSourcegraphURL]) {
+    const url = urlFn()
+    if (url) {
+      return url;
+    }
+  }
+  return null;
+}
+
+function preload(url) {
+  var i = document.createElement('iframe');
+  i.style.display = 'none';
+  i.onload = function() { i.parentNode.removeChild(i); };
+  i.src = url;
+  document.body.appendChild(i);
+}
+
 (function() {
   document.addEventListener('keyup', (event) => {
     if (event.key === "V" && event.ctrlKey && !event.altKey && !event.metaKey && event.shiftKey) {
-      for (const urlFn of [getGitHubPRURL, getGitHubIssueURL, getGitHubCommitURL, getSourcegraphURL]) {
-        const url = urlFn()
-        if (url) {
-          window.open(url, "_blank");
-          return;
-        }
+      const url = getClickCandidate()
+      if (url) {
+        window.open(url, "_blank");
       }
     }
+
+    if (event.key === "F" && event.ctrlKey && !event.altKey && !event.metaKey && event.shiftKey) {
+      setTimeout(() => {
+        const url = getClickCandidate();
+        console.log('url', url);
+        if (url) {
+          preload(url)
+        }
+      }, 5000);
+    }
+
+    
   });
+
+
 })();
