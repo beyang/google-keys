59f3f2f21298ed164db5cb8cfd40751c8222fa5c
Beyang Liu <beyang@sourcegraph.com>
2020-10-02 14:23:58 -0700

    spacing

--- searchkeys.js
+++ searchkeys.js
@@ -1,73 +1,73 @@
 (function() {
-    /**
-     * State
-     */
-    let selected = -1;
-    let results = [];
+  /**
+   * State
+   */
+  let selected = -1;
+  let results = [];
 
-    /**
-     * CSS
-     */
-    let css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = '.quick-selected::before { content: "▶"; position: absolute; left: -1em; font-size: 0.75em; top: 0.125em; }';
-    document.body.appendChild(css);
+  /**
+   * CSS
+   */
+  let css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = '.quick-selected::before { content: "▶"; position: absolute; left: -1em; font-size: 0.75em; top: 0.125em; }';
+  document.body.appendChild(css);
 
-    function isActive() {
-        return document.activeElement.tagName !== "INPUT";
+  function isActive() {
+    return document.activeElement.tagName !== "INPUT";
+  }
+
+  /**
+   * Sync view to state
+   */
+  function refresh() {
+    for (let i = 0; i < results.length; i++) {
+      if (i === selected) {
+        results[i].classList.add("quick-selected");
+        if (!isInViewport(results[i])) {
+          results[i].scrollIntoView(true);
+        }
+      } else {
+        results[i].classList.remove("quick-selected");
+      }
+    }
+  }
+
+  function isInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    var html = document.documentElement;
+    return (
+      rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || html.clientHeight) &&
+        rect.right <= (window.innerWidth || html.clientWidth)
+    );
+  }
+
+  document.addEventListener('keydown', function (event) {
+    if (!isActive()) {
+      return;
     }
 
-    /**
-     * Sync view to state
-     */
-    function refresh() {
-        for (let i = 0; i < results.length; i++) {
-            if (i === selected) {
-                results[i].classList.add("quick-selected");
-                if (!isInViewport(results[i])) {
-                    results[i].scrollIntoView(true);
-                }
-            } else {
-                results[i].classList.remove("quick-selected");
-            }
-        }
+    if (event.key === "Tab" || event.key === "ArrowUp" || event.key === "ArrowDown") {
+      event.preventDefault();
+      results = document.querySelectorAll("div.r > a[href]");
+      if (results.length === 0) {
+        return;
+      }
+      if ((event.key === "Tab" && event.shiftKey) || event.key === "ArrowUp") {
+        selected = (selected - 1 + results.length) % results.length;
+      } else if ((event.key === "Tab" && !event.shiftKey) || event.key === "ArrowDown") {
+        selected = (selected + 1 + results.length) % results.length;
+      }
+      refresh();
+    } else if (event.key === "Enter") {
+      if (results.length === 0) {
+        return;
+      }
+      window.location.href = results[selected].href;
+    } else if (event.key === "Escape") {
+      document.querySelector("input.gsfi").select();
     }
-
-    function isInViewport(element) {
-        var rect = element.getBoundingClientRect();
-        var html = document.documentElement;
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || html.clientHeight) &&
-            rect.right <= (window.innerWidth || html.clientWidth)
-        );
-    }
-
-    document.addEventListener('keydown', function (event) {
-        if (!isActive()) {
-            return;
-        }
-
-        if (event.key === "Tab" || event.key === "ArrowUp" || event.key === "ArrowDown") {
-          event.preventDefault();
-            results = document.querySelectorAll("div.r > a[href]");
-            if (results.length === 0) {
-                return;
-            }
-            if ((event.key === "Tab" && event.shiftKey) || event.key === "ArrowUp") {
-                selected = (selected - 1 + results.length) % results.length;
-            } else if ((event.key === "Tab" && !event.shiftKey) || event.key === "ArrowDown") {
-                selected = (selected + 1 + results.length) % results.length;
-            }
-            refresh();
-        } else if (event.key === "Enter") {
-            if (results.length === 0) {
-                return;
-            }
-            window.location.href = results[selected].href;
-        } else if (event.key === "Escape") {
-            document.querySelector("input.gsfi").select();
-        }
-    }, false);
+  }, false);
 })();
